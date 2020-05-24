aa267f14867d25de01b52ce4089294ad4574d5bd
Danny Ohana <dannyohana@Dannys-MacBook-Air.local>
2020-05-24 15:05:53 -0400

    test2

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,28 +1,40 @@
 let angle = 0;
+let pos = 1;
 let cnv;
 
 function setup() {
-    cnv = createCanvas(windowWidth/2, 400, WEBGL);
-    cnv.position(windowWidth/2-10, 70);
+    // cnv = createCanvas(windowWidth/2, 350, WEBGL);
+    cnv = createCanvas(windowWidth, 350, WEBGL);
+    // cnv.position(windowWidth/2-30, 70);
+    cnv.position(0, 80);
 }
 
 function draw() {
     // background(0);
     background(0, 0, 0, 0);
 
-    rectMode(CENTER);
+    // rectMode(TOP);
     // noStroke();
     stroke(0);
     fill(0, 0, 255);
-    rotateX(-mouseY * 0.003 - 20);
+    rotateX(pos * .01);
+    rotateX(-mouseY * 0.003);
     rotateY(-mouseX * 0.003);
-    box(60, 60, 60)
+    box(60, 60, 60);
 
     angle += 0.07;
 }
 
+// function mouseWheel(event) {
+//     print(event.delta);
+//     //move the square according to the vertical scroll amount
+//     pos += event.delta;
+//     //uncomment to block page scrolling
+//     // return false;
+// }
+
 function windowResized() {
-    console.log(windowWidth);
-    cnv.position(windowWidth/2-10 ,70);
-    resizeCanvas(windowWidth/2, 400);
+    // cnv.position(windowWidth/2-30 ,70);
+    // resizeCanvas(windowWidth/2, 350);
+    resizeCanvas(windowWidth, 350);
   }

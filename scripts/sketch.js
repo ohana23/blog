72179cb239284d42bb4780aa3ed62d57f9b175b6
Danny Ohana <dannyohana@Dannys-MacBook-Air.local>
2020-05-24 19:33:02 -0400

    reduce margin top home main and turn object blue

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -7,31 +7,30 @@ function preload() {
 }
 
 function setup() {
-    cnv = createCanvas(windowWidth, 350, WEBGL);
+    cnv = createCanvas(windowWidth, 300, WEBGL);
     cnv.position(0, 80);
 }
 
 function draw() {
     background(0, 0, 0, 0);
-    ambientLight(234, 241, 255);
+    // ambientLight(234, 241, 255);
 
     noStroke();
 
-    scale(0.3);
+    scale(0.25);
     rotateY(angle);
     rotateX(9.6);
     rotateZ(6.4);
     push();
     translate(330, -400, 0);
-    normalMaterial();
+    // normalMaterial();
+    fill(0, 0, 255, 100);
     model(modelObj);
     pop();
 
-    angle += 0.01;
+    angle += 0.02;
 }
 
 function windowResized() {
-    // cnv.position(windowWidth/2-30 ,70);
-    // resizeCanvas(windowWidth/2, 350);
-    resizeCanvas(windowWidth, 350);
+    resizeCanvas(windowWidth, 300);
   }
